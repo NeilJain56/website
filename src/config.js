767b2c60a7a4d9f2c1f056dda4b4ddb787a56553
NeilJain56 <neiljain56@gmail.com>
2020-05-06 18:23:55 -0400

    Project

--- src/config.js
+++ src/config.js
@@ -1,27 +1,39 @@
 module.exports = {
-  siteTitle: 'Neil Jain | Software Engineer',
+  siteTitle: 'Brittany Chiang | Software Engineer',
   siteDescription:
     'Brittany Chiang is a software engineer based in Boston, MA who specializes in building (and occasionally designing) exceptional websites, applications, and everything in between.',
   siteKeywords:
-    'Neil Jain, Neil, Jain, neiljain56, software engineer, front-end engineer, web developer, javascript, rutgers',
-  siteUrl: '',
+    'Brittany Chiang, Brittany, Chiang, bchiang7, software engineer, front-end engineer, web developer, javascript, northeastern',
+  siteUrl: 'https://brittanychiang.com',
   siteLanguage: 'en_US',
   googleAnalyticsID: 'UA-45666519-2',
   googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
-  name: 'Neil Jain',
-  location: 'North Brunswick, NJ',
-  email: 'neiljain56@gmail.com',
-  github: 'https://github.com/NeilJain56',
+  name: 'Brittany Chiang',
+  location: 'Boston, MA',
+  email: 'brittany.chiang@gmail.com',
+  github: 'https://github.com/bchiang7',
+  twitterHandle: '@bchiang7',
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/NeilJain56',
+      url: 'https://github.com/bchiang7',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/neil-jain-42b255146/',
+      url: 'https://www.linkedin.com/in/bchiang7',
     },
-    
+    {
+      name: 'Codepen',
+      url: 'https://codepen.io/bchiang7',
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/bchiang7',
+    },
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/bchiang7',
+    },
   ],
 
   navLinks: [
